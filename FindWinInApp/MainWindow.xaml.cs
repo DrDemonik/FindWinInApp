69ad96a93c5d3954f6e9fe1cd7137fcd8842b0f3
DrDemonik-pc\DrDemonik <nekrach@gmail.com>
2019-09-30 19:13:33 +0300

    Вывод индификатора дочернего окна и название класса окна, для например отправки сообщения.

--- FindWinInApp/MainWindow.xaml.cs
+++ FindWinInApp/MainWindow.xaml.cs
@@ -23,34 +23,45 @@ namespace FindWinInApp
     public partial class MainWindow : Window
     {
         List<Proc> dataProcs;
-        List<IntPtr> dataWIN;
 
         public MainWindow()
         {
             InitializeComponent();
             dataProcs = new List<Proc>();
+            this.ProcessGrid.ItemsSource = dataProcs;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            dataProcs.Clear();
             var procs = Process.GetProcesses();
             foreach (var proc in procs)
             {
                 if(proc.MainWindowHandle!=IntPtr.Zero)
                     dataProcs.Add( new Proc() { WinHanler=proc.MainWindowHandle, WinName = proc.MainWindowTitle, ProcessName = proc.ProcessName });
             }
-            this.ProcessGrid.ItemsSource = dataProcs;
+            this.ProcessGrid.Items.Refresh();
+            this.ChildsWinGrid.ItemsSource = null;
+            this.ChildsWinGrid.Items?.Refresh();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             var temp = this.ProcessGrid.SelectedItem as Proc;
+            List<Tuple<IntPtr, string>> Items = new List<Tuple<IntPtr, string>>();
             if (temp != null)
             {
                 this.LCurrentProc.Content = temp.ProcessName;
-                 dataWIN = new WindowHandleInfo(temp.WinHanler).GetAllChildHandles();
-                this.ChildsWinGrid.ItemsSource = dataWIN;
-            }            
+                foreach(var item in new WindowHandleInfo(temp.WinHanler).GetAllChildHandles())
+                {
+                    StringBuilder nameClass = new StringBuilder(256);
+                    if (GetClassName(item, nameClass, nameClass.Capacity) != 0)
+                    {
+                        Items.Add(new Tuple<IntPtr, string>(item,nameClass.ToString()));
+                    }
+                }
+            }
+            this.ChildsWinGrid.ItemsSource = Items;
         }
 
         [DllImport("user32.dll", SetLastError = true)]
@@ -64,6 +75,9 @@ namespace FindWinInApp
 
         [DllImport("User32.dll")]
         public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
     }
 
     public class Proc
